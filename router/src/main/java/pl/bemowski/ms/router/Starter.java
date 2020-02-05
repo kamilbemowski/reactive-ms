226366c7ecd151f5c081e6169500eb159b1e7fd0
Kamil Bemowski <kamil@bemowski.pl>
2020-02-05 10:55:11 +0100

    base template project

--- router/src/main/java/pl/bemowski/ms/router/Starter.java
+++ router/src/main/java/pl/bemowski/ms/router/Starter.java
@@ -3,25 +3,11 @@ package pl.bemowski.ms.router;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.web.Router;
-import pl.bemowski.ms.router.handler.AirlineRequestHandler;
-import pl.bemowski.ms.router.handler.FlightsRequestHandler;
-import pl.bemowski.ms.router.handler.PassengerRequestHandler;
-import pl.bemowski.ms.router.handler.RequestHandler;
 
 public class Starter extends AbstractVerticle {
 
 
     private final Logger logger = LoggerFactory.getLogger(Starter.class);
-
-    @Override
-    public void start() {
-        Router router = Router.router(vertx);
-        RequestHandler passengerHandler = new PassengerRequestHandler(vertx);
-        RequestHandler flightsHandler = new FlightsRequestHandler(vertx);
-        RequestHandler airlineHandler = new AirlineRequestHandler(vertx);
-        vertx.createHttpServer().requestHandler(router).listen(8012);
-    }
 
 
 }
