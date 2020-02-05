5297220febd4f80291d6b1fc7fd2ee3e352eba65
Kamil Bemowski <kamil@bemowski.pl>
2020-02-05 10:28:35 +0100

    airline empty

--- router/src/main/java/pl/bemowski/ms/router/handler/FlightsRequestHandler.java
+++ router/src/main/java/pl/bemowski/ms/router/handler/FlightsRequestHandler.java
@@ -4,7 +4,6 @@ import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
-import pl.bemowski.ms.common.model.EventBusAddresses;
 
 public class FlightsRequestHandler implements RequestHandler {
     private final Vertx vertx;
@@ -17,13 +16,5 @@ public class FlightsRequestHandler implements RequestHandler {
     @Override
     public void handle(RoutingContext event) {
         logger.info("Handle flight request");
-        event.request().handler(buffer -> {
-            String flightMessage = buffer.toString();
-            logger.info("Flight message: " + flightMessage);
-            vertx.eventBus()
-                    .publish(EventBusAddresses.FLIGHT, flightMessage).publish(EventBusAddresses.TIMETABLE, flightMessage);
-            event.response().setStatusCode(201).end("ok");
-
-        });
     }
 }
